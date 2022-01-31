a2f631ba4ca920cf2cacf8b54e266ee19e53e2ee
Son <80449601+nsonvt@users.noreply.github.com>
2022-01-31 16:47:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$con=mysqli_connect("localhost","root","","pao_belgische_bier_db");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM bier");
+
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>Naam</th>
+<th>Brouwer</th>
+<th>Type</th>
+<th>Gisting</th>
+<th>Perc</th>
+<th>Inkoop prijs</th>
+
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+
+{
+echo "<tr>";
+echo "<td>" . $row['id'] . "</td>";
+echo "<td>" . $row['naam'] . "</td>";
+echo "<td>" . $row['brouwer'] . "</td>";
+echo "<td>" . $row['type'] . "</td>";
+echo "<td>" . $row['gisting'] . "</td>";
+echo "<td>" . $row['perc'] . "</td>";
+echo "<td>" . $row['inkoop_prijs'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
